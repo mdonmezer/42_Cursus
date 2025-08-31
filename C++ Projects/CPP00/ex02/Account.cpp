546930e1dce30f3838e004dd0a4556aaf2cc5f0a
Muhammet Dönmezer <mdonmeze@k1m30s03.42istanbul.com.tr>
2025-09-01 01:49:16 +0300

    finish

---
+++ C++ Projects/CPP00/ex02/Account.cpp
@@ -0,0 +1,110 @@
+#include "Account.hpp"
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+
+// Static member initialization
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+// Static member functions
+int Account::getNbAccounts(void) {
+    return _nbAccounts;
+}
+
+int Account::getTotalAmount(void) {
+    return _totalAmount;
+}
+
+int Account::getNbDeposits(void) {
+    return _totalNbDeposits;
+}
+
+int Account::getNbWithdrawals(void) {
+    return _totalNbWithdrawals;
+}
+
+void Account::displayAccountsInfos(void) {
+    _displayTimestamp();
+    std::cout << "accounts:" << _nbAccounts << ";total:" << _totalAmount
+              << ";deposits:" << _totalNbDeposits << ";withdrawals:" << _totalNbWithdrawals << std::endl;
+}
+
+// Log file format
+void Account::_displayTimestamp(void) {
+    std::time_t now = std::time(0);
+    std::tm* ltm = std::localtime(&now);
+    
+    std::cout << "[" << (1900 + ltm->tm_year)
+              << std::setfill('0') << std::setw(2) << (1 + ltm->tm_mon)
+              << std::setfill('0') << std::setw(2) << ltm->tm_mday << "_"
+              << std::setfill('0') << std::setw(2) << ltm->tm_hour
+              << std::setfill('0') << std::setw(2) << ltm->tm_min
+              << std::setfill('0') << std::setw(2) << ltm->tm_sec << "] ";
+}
+
+// Constructor
+Account::Account(int initial_deposit) : _accountIndex(_nbAccounts), _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0) {
+    _nbAccounts++;
+    _totalAmount += initial_deposit;
+    
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created" << std::endl;
+}
+
+// Destructor
+Account::~Account(void) {
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";closed" << std::endl;
+}
+
+// Member functions
+void Account::makeDeposit(int deposit) {
+    int p_amount = _amount;
+    _amount += deposit;
+    _nbDeposits++;
+    _totalAmount += deposit;
+    _totalNbDeposits++;
+    
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";p_amount:" << p_amount
+              << ";deposit:" << deposit << ";amount:" << _amount 
+              << ";nb_deposits:" << _nbDeposits << std::endl;
+}
+
+bool Account::makeWithdrawal(int withdrawal) {
+    int p_amount = _amount;
+    
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";p_amount:" << p_amount << ";withdrawal:";
+    
+    if (withdrawal > _amount) {
+        std::cout << "refused" << std::endl;
+        return false;
+    }
+    
+    _amount -= withdrawal;
+    _nbWithdrawals++;
+    _totalAmount -= withdrawal;
+    _totalNbWithdrawals++;
+    
+    std::cout << withdrawal << ";amount:" << _amount 
+              << ";nb_withdrawals:" << _nbWithdrawals << std::endl;
+    return true;
+}
+
+int Account::checkAmount(void) const {
+    return _amount;
+}
+
+void Account::displayStatus(void) const {
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";amount:" << _amount
+              << ";deposits:" << _nbDeposits << ";withdrawals:" << _nbWithdrawals << std::endl;
+}
+
+// Private default constructor (unused)
+Account::Account(void) {
+}
