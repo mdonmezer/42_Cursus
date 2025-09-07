9948b6ffd584f62176557d7f8cbe8c47c721b005
Muhammet Dönmezer <mdonmeze@k1m30s01.42istanbul.com.tr>
2025-09-07 07:23:07 +0300

    finish

--- C++ Projects/CPP00/ex00/megaphone.cpp
+++ C++ Projects/CPP00/ex00/megaphone.cpp
@@ -9,8 +9,6 @@ int main(int ac, char **av)
 	{
 		for (int j = 0; av[i][j]; j++)
 			std :: cout << (char)toupper(av[i][j]);
-		if (i < ac - 1)
-			std :: cout << " ";
 	}
 	std :: cout << std :: endl;
 	return 0;
